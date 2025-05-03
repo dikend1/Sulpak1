85d4a674fc9d8f5d38ddc2d56331870c54fcf8b0
dikend1 <maxatovdias@gmail.com>
2025-05-03 23:43:19 +0500

    update 7

--- One/serializers.py
+++ One/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import CustomUser
 
 
+
 class CustomUserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
--- One/views.py
+++ One/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from serializers import CustomUserRegistrationSerializer
+from One.serializers import CustomUserRegistrationSerializer
 
 
 @api_view(['POST'])
