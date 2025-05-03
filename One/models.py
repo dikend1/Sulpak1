8a9ec3da719b058adaf3d3b9aeeea33e9b6feca2
dikend1 <maxatovdias@gmail.com>
2025-05-03 22:22:24 +0500

    update2 commit

--- One/models.py
+++ One/models.py
@@ -1,19 +1,33 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class Restaurant(AbstractUser):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    qr_url = models.URLField(blank=True,null=True,default="")
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name','password','email']
+
+    def __str__(self):
+        return self.email
 
 # Тамак ушын мысалы салат Основные блюда
 class Category(models.Model):
-    name = models.CharField(max_length=100,verbose_name='Category')
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 # Блюда
 class Dish(models.Model):
-    name = models.CharField(max_length=200,verbose_name='Dish')
-    description = models.TextField(verbose_name='Description')
-    price = models.DecimalField(max_digits=10, decimal_places=2,verbose_name='Price')
-    category = models.ForeignKey(Category, related_name='dishes', on_delete=models.CASCADE,verbose_name='Category')
-    image = models.ImageField(uplaod_to='images/',null = True,blank=True,verbose_name='Image')
+    restaurant = models.ForeignKey(Restaurant, related_name='dishes', on_delete=models.CASCADE, null=True, blank=True, default=None)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.FloatField(null=True, blank=True)
+    category = models.ForeignKey(Category, related_name='dishes', on_delete=models.CASCADE)
+    image = models.URLField(blank=True,null=True,default="")
 
     def __str__(self):
         return self.name
@@ -21,8 +35,32 @@ class Dish(models.Model):
 
 #Заказы
 class Order(models.Model):
-    user_name = models.CharField(max_length=100,verbose_name='Name_client')
-    dishes = models.CharField(Dish, related_name='orders', verbose_name='Dish')
-    total_price = models.DecimalField(max_digits=10, decimal_places=2,verbose_name='Total_price')
-    created_at = models.DateField(auto_now_add=True,verbose_name='Created_at')
-    status = models.CharField(max_length=50,choices=[('pending','в ожидание')])
+    user_name = models.CharField(max_length=100)
+    dishes = models.ManyToManyField(Dish, related_name='orders')
+    total_price = models.FloatField()
+    created_at = models.DateField(auto_now_add=True)
+    number = models.CharField(max_length=12)
+
+    def __str__(self):
+        return f"{self.id} от {self.user_name}"
+
+class Review(models.Model):
+    dish = models.ForeignKey(Dish, related_name='reviews', on_delete=models.CASCADE)
+    user_name = models.CharField(max_length=100)
+    rating = models.IntegerField(choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')])  # Оценка блюда
+    review_text = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Отзыв на {self.dish.name} от {self.user_name}"
+
+
+class DeliveryStatus(models.Model):
+    order = models.ForeignKey(Order, related_name='delivery_statuses', on_delete=models.CASCADE)
+    status = models.CharField(max_length=50, choices=[('Preparing', 'Готовится'), ('Dispatched', 'Отправлено'), ('Delivered', 'Доставлено')])
+    timestamp = models.DateTimeField(auto_now_add=True)  # Время изменения статуса
+
+    def __str__(self):
+        return f"Статус доставки для заказа {self.order.id}: {self.status}"
+
+
---
+++ One/serializers.py
@@ -0,0 +1,8 @@
+# from rest_framework import serializers
+# from .models import Dish,Order,Category
+#
+#
+# class DishSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Dish
+#         fields =
